4a157b5f6f51d95a877757b179909ce4172cf351
shekinahNavarro <navarroshekinah31@gmail.com>
2023-12-11 20:52:35 -0500

    practica

---
+++ ejercicios/modulo.js
@@ -0,0 +1,17 @@
+/* creamos una funcion que nos retorne la potencia de 2
+ poweroftwo(2) -> 4
+ poweoftwo(4) -> 16
+ poweroftwo(10) -> 100 
+ */
+function poweroftwo(expo){
+    return 2**exp;
+}
+
+let poweroftwo=exp => 2** exp;
+//AREA DEL CIRCULO
+function areaCirculo(r) {
+    return 3.1415*(r**2);
+}
+
+let areaCirculo = r=> 3.1415*(r**2); 
+export { poweroftwo }
---
+++ ejercicios/script.js
@@ -0,0 +1,4 @@
+import{poweroftwo} from "./modulo.js";
+let num = promp("ingresar exponente:");
+confirm.log(poweroftwo(num));
+
